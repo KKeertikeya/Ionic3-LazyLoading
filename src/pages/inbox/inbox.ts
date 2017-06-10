5679882e3d536d7240bcd9bfe3ca6ad9805e6148
Keertikeya <keertikeya.gupta@eclincher.com>
2017-06-09 17:50:15 -0700

    Multiple messages in inbox page

--- src/pages/inbox/inbox.ts
+++ src/pages/inbox/inbox.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-// import { MessageComponent } from '../../components/message/message';
+import { MessageComponent } from '../../components/message/message';
 
 @Component({
   selector: 'page-inbox',
   templateUrl: 'inbox.html'
 })
+
+
 export class InboxPage {
+  messages = [MessageComponent, MessageComponent];
+
   constructor(public navCtrl: NavController) {
 
   }
